b3680ce64da9a9aedd65718a39847680655994d0
Oleksandr Chernookov <chernookov@mail.ru>
2017-07-24 22:37:31 +0200

    removed classes for resources folder, updated git ignore

---
+++ PatternRecognition/src/main/java/Merging.java
@@ -0,0 +1,54 @@
+public class Merging {
+
+    public static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
+        assert isSorted(a, lo, mid); // precondition: a[lo..mid] sorted
+        assert isSorted(a, mid + 1, hi); // precondition: a[mid+1..hi] sorted
+
+        // copy
+        for (int k = lo; k <= hi; k++)
+            aux[k] = a[k];
+
+        // merge
+        int i = lo, j = mid + 1;
+        for (int k = lo; k <= hi; k++) {
+            if (i > mid) a[k] = aux[j++];
+            else if (j > hi) a[k] = aux[j++];
+            else if (less(aux[j], aux[i])) a[k] = aux[j++];
+            else a[k] = aux[i++];
+        }
+
+        assert isSorted(a, lo, hi);
+    }
+
+    private static boolean less(Comparable aux, Comparable aux1) {
+        return false;
+    }
+
+
+    private static boolean isSorted(Comparable[] a, int i, int hi) {
+        return false;
+    }
+
+    public static void sort(Comparable[] a, Comparable[] aux, int lo, int hi) {
+        if (hi <= lo) return;
+        int mid = lo + (hi - lo) / 2;
+        sort(a, aux, lo, mid);
+        sort(a, aux, mid + 1, hi);
+        if (!less(a[mid + 1], a[mid])) return;
+        merge(a, aux, lo, mid, hi);
+    }
+
+    public static void sort(Comparable[] a) {
+        Comparable[] aux = new Comparable[a.length];
+        sort(a, aux, 0, a.length - 1);
+    }
+
+    public static void sort1(Comparable[] a) {
+        int N = a.length;
+        Comparable[] aux = new Comparable[N];
+        for (int sz = 1; sz < N; sz = sz + sz)
+            for (int lo = 0; lo < N - sz; lo += sz + sz)
+                merge(a, aux, lo, lo + sz - 1, Math.min(lo + sz + sz - 1, N - 1));
+
+    }
+}
